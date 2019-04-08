00cb76c135460d989cad3bc1d9d511e843ab5e85
johnson_891609 <johnson_891609@VHC41716D7168D.leanderisd.org>
2019-04-08 13:44:01 -0500

    Fixing grid locations

--- Yahtzee/src/application/game/Table.java
+++ Yahtzee/src/application/game/Table.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
@@ -20,13 +21,15 @@ import javafx.scene.text.Text;
 
 public class Table extends Group{
 	private double baseHeight = 390;
-	private double baseWidth = 450;
+	private double baseWidth = 448;
 	private StackPane tableView;
 	private GridPane diceGrid;
 	private Game game;
 	private VBox container;
 	private ArrayList<Dice> fieldDice;
 	private ImageView tablePic;
+	
+
 	
 	public Table(Game g) throws FileNotFoundException {
 		game = g;
@@ -40,17 +43,17 @@ public class Table extends Group{
 		tableView = new StackPane();
 		
 		diceGrid = new GridPane();
-		diceGrid.setHgap(5);
-		diceGrid.setVgap(5);
+		diceGrid.setHgap(6*game.scaleFactorX);
+		diceGrid.setVgap(6*game.scaleFactorY);
 		
 		tableView.getChildren().add(tablePic);
 		tableView.getChildren().add(diceGrid);
 	
+		diceGrid.setStyle("-fx-grid-lines-visible: true;");
 		container.getChildren().add(tableView);
 		this.getChildren().add(container);
 		
 		rescaleSizes();
-	
 	}
 	
 	protected void rescaleSizes() {
@@ -62,10 +65,31 @@ public class Table extends Group{
 		
 		tablePic.setFitHeight(baseHeight * game.scaleFactorY);
 		tablePic.setFitWidth(baseWidth * game.scaleFactorX);
+		diceGrid.setPadding(new Insets(25*game.scaleFactorY, 0, 0, 20*game.scaleFactorX));
+		diceGrid.setHgap(6*game.scaleFactorX);
+		diceGrid.setVgap(6*game.scaleFactorY);
 		
 	}
 	
 	protected void renderDice() {
+		ArrayList<Node> addDice = new ArrayList<Node>();
+		for(Node d : diceGrid.getChildren()) {
+			if(d instanceof Dice) {
+			((Dice)d).setBaseHeight(45);
+			((Dice)d).setBaseWidth(45);
+			addDice.add(d);
+			}
+		}
+		diceGrid.getChildren().removeAll(addDice);
+		for(Node d : addDice) {
+			if(d instanceof Dice) {
+			((Dice)d).setBaseHeight(45);
+			((Dice)d).setBaseWidth(45);
+			//Y RANGE: 18-30
+			//X RANGE: 12-50
+			diceGrid.add(d,(int)(Math.random()*13)+18,(int)(Math.random()*39)+12);
+			}
+		}
 		
 	}
 	
@@ -77,8 +101,9 @@ public class Table extends Group{
 		d.setBaseHeight(80);
 		d.setBaseWidth(80);
 		fieldDice.add(d);
-		diceGrid.setPadding(new Insets(25*game.scaleFactorY, 0, 0, (tablePic.getFitWidth()/2.3)-(d.getWidth()*(fieldDice.size()-1))));
-		diceGrid.add(d, fieldDice.indexOf(d), 0);
+		diceGrid.setPadding(new Insets(25*game.scaleFactorY, 0, 0, 20*game.scaleFactorX));
+		diceGrid.add(d,fieldDice.size(), 0);
+	
 	
 	}
 	
@@ -87,6 +112,7 @@ public class Table extends Group{
 		d.setBaseWidth(100);
 		fieldDice.remove(d);
 		diceGrid.getChildren().remove(d);
+		
 	}
 	
 	
